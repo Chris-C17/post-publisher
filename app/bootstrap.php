d2175619d6f4c7463ecf7b532de2c1501f6dc41e
Christopher Chambers <christopher.chambers@live.co.uk>
2018-11-19 18:03:15 +0000

    Created config files and now autoloading Core Libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,7 +6,16 @@
  * Time: 20:26
  */
 
-# Load Libraries (not case sensitive)
-require_once 'libraries/CoreController.php';
-require_once 'libraries/BaseController.php';
-require_once 'libraries/DatabaseController.php';
+# Load config
+require_once 'config/config.php';
+
+//# Load Libraries (not case sensitive)
+//require_once 'libraries/CoreController.php';
+//require_once 'libraries/BaseController.php';
+//require_once 'libraries/DatabaseController.php';
+
+# Autoload Core Libraries (same as above)
+spl_autoload_register(function ($className)
+{
+    require_once 'libraries/' . $className . '.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: c_chambers
+ * Date: 19/11/2018
+ * Time: 16:56
+ */
+
+# App Route - define app directory as a constant
+define('APPROOT', dirname(dirname(__FILE__)));
+# URL Root
+define('URLROOT', 'http://localhost/mvc/public');
+# Site Name
+define('SITENAME', 'MVC');
